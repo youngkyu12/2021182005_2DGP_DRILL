012d0375074f01ee08a1ace5affbf3a3b2629519
youngkyu12 <112385423+youngkyu12@users.noreply.github.com>
2022-09-21 11:48:01 +0900

    circle_move

--- Drill06/drill6.py
+++ Drill06/drill6.py
@@ -9,7 +9,7 @@ character = load_image('character.png')
 
 x = 0
 y = 90
-while (x < 800):
+while (x < 790):
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(x, y)
@@ -35,9 +35,22 @@ while (y >= 510):
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(x, y)
-    y = y + 2
+    y = y - 2
     delay(0.01)
 
 # circle_move
+
+x = 1
+y = 1
+
+while (x <= 360):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(400 + (270 * math.sin(x / 360 * 2 * math.pi)), 360 - (270 * math.cos(y / 360 * 2 * math.pi)))
+    y = y + 1
+    x = x + 1
+    delay(0.01)
+
+
 
 close_canvas()
