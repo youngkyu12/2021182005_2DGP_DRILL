21b3b2649a07424d8bda513b980545a10b44acc3
youngkyu12 <112385423+youngkyu12@users.noreply.github.com>
2022-10-17 13:45:27 +0900

    취소

--- Game/main.py
+++
@@ -1,9 +0,0 @@
-import game_framework
-import pico2d
-
-import main_state
-Width, Height = 1280, 720
-pico2d.open_canvas(Width, Height)
-pico2d.hide_lattice()
-game_framework.run(main_state)
-pico2d.close_canvas()
